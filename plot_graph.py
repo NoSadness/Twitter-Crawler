b348e0375f6080989835afc3ed589225ea134d2e
Yushan Feng <brandon.fengys@gmail.com>
2017-04-27 15:25:53 -0400

    Add files via upload

--- plot_graph.py
+++ plot_graph.py
@@ -15,24 +15,69 @@ for (twitter_user, followed_by, followers) in twitter_network:
     hfollowers[twitter_user] = int(followers)
     orig_users.add(twitter_user)
 
-g = o
-colors = []
-d = net.degree(g)
-for n in g.nodes():
-    if d[n] <= 2:
-        g.remove_node(n)
-    else:
-        if n in orig_users:
-            colors.append('red')
-        else:
-            colors.append('black')
+SEED = 'CNN'
+g = net.DiGraph(net.ego_graph(o, SEED , radius=3))
 
-labels = {}
-for n in g.nodes():
-    if n in orig_users:
-        labels[n] = n
+def trim_degrees_ted(g, degree=1, ted_degree=1):
+    g2 = g.copy()
+    d = net.degree(g2)
+    for n in g2.nodes():
+        if n == SEED: continue # don't prune the SEED node
+        if n not in orig_users:
+            g2.remove_node(n)
+    return g2
 
-net.draw_networkx_labels(g,pos=net.spring_layout(g))
-net.draw_networkx(g, node_color=colors)
+
+def trim_edges_ted(g, weight=1, ted_weight=10):
+    g2 = net.DiGraph()
+    for f, to, edata in g.edges_iter(data=True):
+        if f == SEED or to == SEED: # keep edges that link to the SEED node
+            g2.add_edge(f, to, edata)
+        elif f in orig_users or to in orig_users:
+            if edata['followers'] >= ted_weight:
+                g2.add_edge(f, to, edata)
+        elif edata['followers'] >= weight:
+            g2.add_edge(f, to, edata)
+    return g2
+
+core = trim_degrees_ted(g, degree=300, ted_degree=1)
+
+core = trim_edges_ted(core, weight=250000, ted_weight=35000)
+
+nodeset_types = { 'Orig': lambda s: s in orig_users, 'Not Orig': lambda s: s not in orig_users }
+
+nodesets = defaultdict(list)
+
+for nodeset_typename, nodeset_test in nodeset_types.iteritems():
+    nodesets[nodeset_typename] = [ n for n in core.nodes_iter() if nodeset_test(n) ]
+
+pos = net.spring_layout(core) # compute layout
+
+colours = ['red','green']
+colourmap = {}
+
+plt.figure(figsize=(18,18))
+plt.axis('off')
+
+# draw nodes
+i = 0
+alphas = {'Orig': 0.6, 'Not Orig': 0.4}
+for k in nodesets.keys():
+    ns = [ math.log10(hfollowers[n]+1) * 80 for n in nodesets[k] ]
+    print k, len(ns)
+    net.draw_networkx_nodes(core, pos, nodelist=nodesets[k], node_size=ns, node_color=colours[i], alpha=alphas[k])
+    colourmap[k] = colours[i]
+    i += 1
+print 'colourmap: ', colourmap
+
+net.draw_networkx_edges(core, pos, width=0.2, alpha=0.2)
+
+# draw labels
+alphas = { 'Orig': 1.0, 'Not Orig': 0.5}
+for k in nodesets.keys():
+    for n in nodesets[k]:
+        x, y = pos[n]
+        plt.text(x, y+0.02, s=n, alpha=alphas[k], horizontalalignment='center', fontsize=9)
+
 plt.savefig("Graph.png", format="PNG")
 plt.show()
